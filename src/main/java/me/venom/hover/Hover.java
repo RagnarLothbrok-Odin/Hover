6e92ce48eb4ff8049804d172d884e023ea0d57f0
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2021-08-08 16:40:58 +0100

    init

---
+++ src/main/java/me/venom/hover/Hover.java
@@ -0,0 +1,17 @@
+package me.venom.hover;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hover extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
